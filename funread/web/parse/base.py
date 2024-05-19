eb02ba7dfff3e18f7d149cc313b9d5530a0f7d92
farfun <1007530194@qq.com>
2024-05-19 23:23:50 +0800

    add

--- funread/web/parse/base.py
+++ funread/web/parse/base.py
@@ -1,6 +1,6 @@
 class BaseParse:
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, source=None, *args, **kwargs):
+        self.source = source
 
     def parse_list(self, page_no, page_size=10, *args, **kwargs):
         pass
